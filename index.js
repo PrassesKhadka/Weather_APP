5dc1aa6bdcb9614c57a5430759a821787b18bc06
Prasses Khadka <khadkaprasses@gmail.com>
2023-03-31 16:24:54 +0545

    Added template for the site and created function returning object->only the api data needed

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-// let city=prompt('Enter a name of a city')
+function return_object(temp_c,temp_f,humidity,condition,country,localtime,name)
+{
+    return {temp_c,temp_f,humidity,condition,country,localtime,name}
+}
+const city='kathmandu';
 async function weather(){
-    const response=await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2a0beee6fdba43eeb3e33624231903&q=kathmandu&days=1&aqi=no&alerts=no`,
+    const response=await fetch(`https://api.weatherapi.com/v1/forecast.json?key=2a0beee6fdba43eeb3e33624231903&q=${city}&days=1&aqi=no&alerts=no`,
     {
         mode:'cors'
     });
     const json=await response.json()
     console.log(json)
-    console.log(json.current.temp_c)
-    console.log(json.current.temp_f)
-    console.log(json.current.humidity)
-    console.log(json.current.condition.text)
-    console.log(json.location.country)
-    console.log(json.location.localtime)
-    console.log(json.location.name)
-
+    const current=json.current
+    const location=json.location
+    const obj=new return_object(current.temp_c,current.temp_f,current.humidity,current.condition.text,location.country,location.localtime,location.name)
+    console.log(obj.temp_c)
 }
 weather()
+
